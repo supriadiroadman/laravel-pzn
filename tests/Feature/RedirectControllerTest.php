d104d15efd5e5c1bd828706814ff968770ff3706
Supriadi Roadman <supriadiroadman@gmail.com>
2022-06-04 14:40:56 +0700

    Redirect

---
+++ tests/Feature/RedirectControllerTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RedirectControllerTest extends TestCase
+{
+    public function testRedirect()
+    {
+        $this->get('/redirect/from')
+            ->assertRedirect('/redirect/to');
+    }
+
+    public function testRedirectName()
+    {
+        $this->get('/redirect/name')
+            ->assertRedirect('/redirect/name/Adi');
+    }
+
+    public function testRedirectAction()
+    {
+        $this->get('/redirect/action')
+            ->assertRedirect('/redirect/name/Adi');
+    }
+
+    public function testRedirectAway()
+    {
+        $this->get('/redirect/pzn')
+            ->assertRedirect('https://www.programmerzamannow.com/');
+    }
+
+
+}
